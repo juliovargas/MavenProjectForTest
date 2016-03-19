7824c28bf9502180ef8194e93990444c3dcdac9b
Julio Vargas <julio.vargasp@gmail.com>
2016-03-19 21:03:29 +0100

    changes in HelloWorld

--- MavenProjectForTest/src/main/java/com/julio/HelloWorld.java
+++ MavenProjectForTest/src/main/java/com/julio/HelloWorld.java
@@ -1,11 +1,25 @@
 package com.julio;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.julio.classes.Person;
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		
+		Person p1 = new Person ("pepe", 24);
+		Person p2 = new Person ("juan", 21);
 		
+		List<Person> l = new ArrayList<Person>();
+		
+		l.add(p1);
+		l.add(p2);
+		
+		for (Person p : l) {
+			System.out.println("Name: " + p.getName());
+		}
 	}
-
 }

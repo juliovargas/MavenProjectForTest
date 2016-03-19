8b7d5ec78825c8a3abe8523c7f4780d351f26244
Julio Vargas <julio.vargasp@gmail.com>
2016-03-19 20:35:48 +0100

    first commit

---
+++ MavenProjectForTest/src/main/java/com/julio/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.julio;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		
+		
+	}
+
+}
---
+++ MavenProjectForTest/src/test/java/com/julio/junit/BasicTest.java
@@ -0,0 +1,14 @@
+package com.julio.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BasicTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
